92ee9b218f03df3f7c0faceb96e1b40181d52b75
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-03-28 23:47:56 +0800

    Update sdl2_piano.c
    re-write the clean function.

--- sdl2_piano.c
+++ sdl2_piano.c
@@ -229,16 +229,15 @@ void init(void){
 }
 
 void clean(void){
-	if (window != NULL){
-		SDL_DestroyWindow(window);
-	}
-
 	int i;
 	for (i = 0; i < PIANO_KEY_NUM; ++i){
 		Mix_FreeChunk(pianoKeys[i].chunk);
 		SDL_DestroyTexture(pianoKeys[i].keyNameTexture);
 		SDL_DestroyTexture(pianoKeys[i].toneNameTexture);
 	}
+	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 
 	Mix_CloseAudio();
 	TTF_CloseFont(font);
